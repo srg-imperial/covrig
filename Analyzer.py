dfc205f0d67e0ed46eebb0b69ea90d5e12b7b739
manlio <manlio.poltronieri@gmail.com>
2013-08-03 21:10:47 +0000

    Introduced Analyzer() helper class
    Provides two methods for now:
    Sketch() -> uses matplotlib to plot a graph from a CSV database
    ZeroCoverage() -> looks for commits with 0 ELOCs

---
+++ Analyzer.py
@@ -0,0 +1,77 @@
+import csv
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+
+class Sketch(object):
+    """ Sketch a raw graph """
+    
+    # fields are:
+    # id, eloc, coverage, t_size, author, edited_lines, covered_lines,
+    #   patch_coverage, timestamp, exitStatus
+
+    def __init__(self, _ifile, _field, _oname):
+        self.ifile = _ifile
+        self.field = _field
+        self.oname = _oname
+
+    def plot(self):
+        res = csv.reader(open(self.ifile), delimiter=',')
+        # list of all the values
+        m = []
+        for value in res:
+            m.append(value[self.field])
+        plt.plot(m)
+        plt.savefig(self.oname)
+        plt.clf()
+
+
+
+class ZeroCoverage(object):
+    """ get a list with all the commits which have 0 ELOCs """
+    def __init__(self, _ifile):
+        self.ifile = _ifile
+
+    def compute(self):
+        res = csv.reader(open(self.ifile), delimiter=',')
+        self.zerocov = []
+        counter = 0
+        for value in res:
+            counter += 1
+            if value[1] == '0':
+                self.zerocov.append(value[0])
+
+        # uncomment for some stats
+        # print self.zerocov
+        # print len(self.zerocov)
+        # print counter
+        # print round((len(self.zerocov) / float(counter))*100,2)
+        # print
+
+
+
+def main():
+    # just some debug cases:
+
+    r = Sketch('plot/data/Redis/Redis.csv', 1, 'redis-coverage.png')
+    r.plot()
+    
+    m = Sketch('plot/data/Memcached/Memcached.csv', 1, 'memcached-coverage.png')
+    m.plot()
+
+    z = Sketch('plot/data/Zeromq/Zeromq.csv', 1, 'zeromq-coverage.png')
+    z.plot()
+
+    rr = GetZeroCoverage('plot/data/Redis/Redis.csv')
+    rr.compute()
+
+    mm = GetZeroCoverage('plot/data/Memcached/Memcached.csv')
+    mm.compute()
+
+    zz = ZeroCoverage('plot/data/Zeromq/Zeromq.csv')
+    zz.compute()
+
+
+if __name__== "__main__":
+    main()
