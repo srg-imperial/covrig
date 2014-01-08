f5c6b498bc7e805eea52be6533fd631409ed9e06
Paul Marinescu <paul.marinescu@imperial.ac.uk>
2014-01-08 14:27:04 +0000

    Breakdown fixes/faults by the type of files they affect

--- postprocessing/faultcoverage-multiple.sh
+++ postprocessing/faultcoverage-multiple.sh
@@ -3,8 +3,20 @@
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 >tmpfaultcov
+OE=0
+OT=0
+TE=0
+
 for SHA in $(sort -u $2); do
   $SCRIPT_DIR/faultcoverage.sh $1 $SHA $3 >> tmpfaultcov
+if [[ $# -gt 3 ]]; then
+    ONLYEXEC=$(grep $SHA $4|awk 'BEGIN { FS="," } ; { if (($7 > 0 || $8 > 0) && $26 == 0) print 1 }'|wc -l)
+    ONLYTEST=$(grep $SHA $4|awk 'BEGIN { FS="," } ; { if ($7 == 0 && $8 == 0 && $26 > 0) print 1 }'|wc -l)
+    TESTANDEXECUTABLE=$(grep $SHA $4|awk 'BEGIN { FS="," } ; { if (($7 > 0 || $8 > 0) && $26 > 0) print 1 }'|wc -l)
+    (( OE += ONLYEXEC ))
+    (( OT += ONLYTEST ))
+    (( TE += TESTANDEXECUTABLE ))
+  fi
 done
 
 FIXES=$(sort -u $2 | wc -l)
@@ -16,3 +28,7 @@ FULLYCOVERED=$(grep 'fullycovered 1' tmpfaultcov | wc -l)
 
 echo "Looked at $FIXES faults ($UNHANDLED unhandled): $COV lines covered, $NOTCOV lines not covered"
 echo "$NOEXEC faults did not change/remove code, $FULLYCOVERED bugs were fully covered"
+
+if [[ $# -gt 3 ]]; then
+  echo "only tests/only code/tests and code $OT/$OE/$TE"
+fi
--- postprocessing/fixcoverage-multiple.sh
+++ postprocessing/fixcoverage-multiple.sh
@@ -3,8 +3,20 @@
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 
 >tmpfixcov
+OE=0
+OT=0
+TE=0
+
 for SHA in $(sort -u $2); do
   $SCRIPT_DIR/fixcoverage.sh $1 $SHA $3 >> tmpfixcov
+  if [[ $# -gt 3 ]]; then
+    ONLYEXEC=$(grep $SHA $4|awk 'BEGIN { FS="," } ; { if (($7 > 0 || $8 > 0) && $26 == 0) print 1 }'|wc -l)
+    ONLYTEST=$(grep $SHA $4|awk 'BEGIN { FS="," } ; { if ($7 == 0 && $8 == 0 && $26 > 0) print 1 }'|wc -l)
+    TESTANDEXECUTABLE=$(grep $SHA $4|awk 'BEGIN { FS="," } ; { if (($7 > 0 || $8 > 0) && $26 > 0) print 1 }'|wc -l)
+    (( OE += ONLYEXEC ))
+    (( OT += ONLYTEST ))
+    (( TE += TESTANDEXECUTABLE ))
+  fi
 done
 
 FIXES=$(sort -u $2 | wc -l)
@@ -16,3 +28,6 @@ FULLYCOVERED=$(grep 'fullycovered 1' tmpfixcov | wc -l)
 
 echo "Looked at $FIXES fixes ($UNHANDLED unhandled): $COV lines covered, $NOTCOV lines not covered"
 echo "$NOEXEC fixes did not change/add code, $FULLYCOVERED fixes were fully covered"
+if [[ $# -gt 3 ]]; then
+  echo "only tests/only code/tests and code $OT/$OE/$TE"
+fi
