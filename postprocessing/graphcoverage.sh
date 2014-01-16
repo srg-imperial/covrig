463eee3161480a6d04a8c2ebcf7a19d42288882f
Paul Marinescu <paul.marinescu@imperial.ac.uk>
2014-01-16 14:48:23 +0000

    Make yaxis [0 : 100]

--- postprocessing/graphcoverage.sh
+++ postprocessing/graphcoverage.sh
@@ -15,13 +15,15 @@ mkdir -p tmp
 #OUTPUT should be graphs/...
 DATAFILE="tmp/${OUTPUT:7}"
 
-egrep -v "$IGNOREREVS" $INPUT |awk 'BEGIN { FS="," } ; { if ($2 > 0) print NR,$3/$2; }' >$DATAFILE
+egrep -v "$IGNOREREVS" $INPUT |awk 'BEGIN { FS="," } ; { if ($2 > 0) print NR,$3*100/$2; }' >$DATAFILE
 
 SCRIPT_DIR="$( cd "$( dirname "$0" )" && pwd )"
 if [[ $GRAPHTYPE == "standalone" ]]; then
   "$SCRIPT_DIR/internal/lineplot.sh" $DATAFILE "$OUTPUT" "Revision" "Coverage" "set size 0.8,0.8
-set title \"${OUTPUT:11}\"" $GRAPHTYPE
+set title \"${OUTPUT:11}\"
+set yrange [ 0 : 100 ]" $GRAPHTYPE
 else
-  "$SCRIPT_DIR/internal/lineplot.sh" $DATAFILE "$OUTPUT" "Revision" "Coverage" "set title \"${OUTPUT:11}\"" $GRAPHTYPE
+  "$SCRIPT_DIR/internal/lineplot.sh" $DATAFILE "$OUTPUT" "Revision" "Coverage" "set title \"${OUTPUT:11}\"
+set yrange [ 0 : 100 ]" $GRAPHTYPE
 fi
 #rm -f $DATAFILE
