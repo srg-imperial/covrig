26783812662363ba4724f95f0328ab3f33e8e576
tomfbailey <thomas.bailey0@outlook.com>
2023-04-02 16:43:36 +0100

    T2: Updated Lighttpd2 code to run containers successfully

--- _Lighttpd2.py
+++ _Lighttpd2.py
@@ -12,7 +12,7 @@ class Lighttpd2(Container):
 
         # set variables
         if self.offline:
-            self.path = self.conn.local("realpath 'repos/lighttpd2'", capture=True)
+            self.path = self.conn.local("realpath 'repos/lighttpd2'", capture=True).stdout.strip()
         else:
             self.path = '/home/lighttpd2'
             self.source_path = '/home/lighttpd2/src'
@@ -25,15 +25,20 @@ class Lighttpd2(Container):
     def compile(self):
         """ compile Lighttpd """
         with self.conn.cd(self.path):
+            # if before or on revision 772e66b91c0c371f5777d50703b3629caa770e6e, remove libev-dev and libev4
             result = self.conn.run('git rev-list 772e66b91c0c371f5777d50703b3629caa770e6e | grep $(git rev-parse HEAD)',
                                    warn=True)
             if result.ok:
                 self.conn.run('apt-get -y remove libev4 libev-dev', warn=True)
-                # run('wget http://dist.schmorp.de/libev/Attic/libev-3.9.tar.gz')
-                with self.conn.cd('/root/libev-3.9'):
-                    self.conn.run('sh configure && make install && ldconfig', warn=True)
+                with self.conn.cd('/root'):
+                    self.conn.run('apt-get install wget && wget http://dist.schmorp.de/libev/Attic/libev-3.9.tar.gz')
+                    self.conn.run('tar -xvf libev-3.9.tar.gz')
+                    self.conn.run('cd libev-3.9 && sh configure && make install && ldconfig', warn=True)
+            # remove -Werror from configure.ac since we're just trying to get coverage data, not ship code
+            self.conn.run('sed -i "s/-Werror\s//g" configure.ac', warn=True)
+            # self.conn.run('sed -i "s/\s--quiet\"/\"/g" configure', warn=True)
             result = self.conn.run('sh autogen.sh && ' +
-                                   'sh configure --with-lua --with-openssl --with-zlib --with-bzip2 &&' +
+                                   'sh configure --with-lua --with-gnutls --with-openssl --with-zlib --with-bzip2 &&' +
                                    " make -j3 CFLAGS='-fprofile-arcs -ftest-coverage -O0 -lm -std=c99' "
                                    + "LDFLAGS='-fprofile-arcs -ftest-coverage'", warn=True)
             if result.failed:
