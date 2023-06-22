b09bdd2e3003719e91bc5e1e8fbb5f7bf689e56b
tomfbailey <thomas.bailey0@outlook.com>
2023-06-22 12:22:33 +0100

    T5: Updated setup file

--- utils/setup.sh
+++ utils/setup.sh
@@ -2,29 +2,35 @@
 
 # Prerequisites:
 
-# e.g. tfb19
+# Take in a username, e.g. abc123
 UNAME=$1
-# Oneliner can also be done with export UNAME=tfb19, etc.:
-# sudo apt-get install -y ufw && sudo ufw allow 80 && sudo ufw allow 22 && sudo addgroup $UNAME && sudo usermod -a -G sudo $UNAME && sudo usermod -a -G ssh $UNAME && sudo mkdir /home/$UNAME && sudo chown $UNAME:$UNAME /home/$UNAME && mkdir /home/$UNAME/.ssh && sudo chown $UNAME:$UNAME /home/$UNAME/.ssh
+
+# Setup for Imperial DoC VMs
 
 # sudo apt-get install -y ufw
 # sudo ufw allow 80
 # sudo ufw allow 22
 # sudo addgroup $UNAME
-# useradd tfb19 -g tfb19
+# useradd ${UNAME} -g ${UNAME}
 # sudo usermod -a -G sudo $UNAME
 # sudo usermod -a -G ssh $UNAME
-# passwd tfb19
+# passwd ${UNAME}
 # sudo mkdir /home/$UNAME
 # sudo chown $UNAME:$UNAME /home/$UNAME
 # mkdir /home/$UNAME/.ssh
 # sudo chown $UNAME:$UNAME /home/$UNAME/.ssh
 
 # From local machine
+# For the machine to ssh into docker containers
 # scp ~/.ssh/id_rsa $UNAME@cloud-vm-XX-XX.doc.ic.ac.uk:/home/$UNAME/.ssh/id_rsa
-# ssh-copy-id -i /home/tom/.ssh/id_ed25519.pub tfb19@45.77.88.150
+
+# For ssh'ing into the machine
+# ssh-copy-id -i /home/tom/.ssh/id_ed25519.pub ${UNAME}@...
 
 # git clone https://github.com/srg-imperial/covrig.git && cd covrig && git switch dev
+
+
+# Then run this script on the VM:
 
 sudo apt-get install -y python3.8-venv
 python3.8 -m venv venv
@@ -51,7 +57,7 @@ sudo apt-get install -y parallel
 
 sudo usermod -a -G docker $UNAME
 
-# reboot
+# Then reboot to update permissions for docker
 
 # cd covrig
 # screen -S sessionX
